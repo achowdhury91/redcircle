77e5b60b3a13247c3c6850d090aa566c7412336f
Abhishek Chowdhury <achowdhury91@gmail.com>
2020-09-11 12:06:46 -0700

    App: cleanup

--- src/App.js
+++ src/App.js
@@ -8,33 +8,6 @@ import {
   CharactersContextProvider,
 } from './reducers/charactersReducer'
 
-// const getAllCharacters = async () => {
-//   const raw = await fetch('https://rickandmortyapi.com/api/character/')
-//   const jsonResponse = await raw.json()
-//
-//   console.log(jsonResponse)
-//
-//   return jsonResponse
-// }
-//
-// class App extends React.Component {
-//   componentDidMount() {
-//     console.log('hi')
-//
-//     fetch('https://rickandmortyapi.com/api/character/')
-//       .then(res => res.json())
-//       .then(res => console.log(res))
-//   }
-//
-//   render() {
-//     return (
-//       <div className='App'>
-//         test
-//       </div>
-//     )
-//   }
-// }
-
 const App = () => {
   const [state, dispatch] = useReducer(charactersReducer, initialState)
 
