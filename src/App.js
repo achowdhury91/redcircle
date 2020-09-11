58e71c1837da2b58e89ccc1751d21ba1f507ce01
Abhishek Chowdhury <achowdhury91@gmail.com>
2020-09-11 11:28:22 -0700

    barebones SearchContainer

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import 'App.css'
+import SearchContainer from './components/SearchContainer/SearchContainer'
 
-const getAllCharacters = async () => {
-  const raw = await fetch('https://rickandmortyapi.com/api/character/')
-  const jsonResponse = await raw.json()
+// const getAllCharacters = async () => {
+//   const raw = await fetch('https://rickandmortyapi.com/api/character/')
+//   const jsonResponse = await raw.json()
+//
+//   console.log(jsonResponse)
+//
+//   return jsonResponse
+// }
+//
+// class App extends React.Component {
+//   componentDidMount() {
+//     console.log('hi')
+//
+//     fetch('https://rickandmortyapi.com/api/character/')
+//       .then(res => res.json())
+//       .then(res => console.log(res))
+//   }
+//
+//   render() {
+//     return (
+//       <div className='App'>
+//         test
+//       </div>
+//     )
+//   }
+// }
 
-  console.log(jsonResponse)
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='content'>
+        <div className='panel'>
+          <SearchContainer />
+        </div>
 
-  return jsonResponse
-}
-
-class App extends React.Component {
-  componentDidMount() {
-    console.log('hi')
-
-    fetch('https://rickandmortyapi.com/api/character/')
-      .then(res => res.json())
-      .then(res => console.log(res))
-  }
-
-  render() {
-    return (
-      <div className='App'>
-        test
+        <div className='panel'>
+          test
+        </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/SearchContainer/SearchContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchContainer = () => {
+  return (
+    <div className='SearchContainer'>
+      SearchContainer
+    </div>
+  )
+}
+
+export default SearchContainer
