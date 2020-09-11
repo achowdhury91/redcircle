64641d41de8bef1464f44c21a3d1e0c4168c37d6
Abhishek Chowdhury <achowdhury91@gmail.com>
2020-09-11 12:57:52 -0700

    styling

--- src/components/CharacterDetail/CharacterDetail.jsx
+++ src/components/CharacterDetail/CharacterDetail.jsx
@@ -6,14 +6,38 @@ const CharacterDetail = ({
   name,
   status,
   species,
-  location,
+  location = {},
 }) => {
+  const {
+    name: locationName,
+  } = location
+
   return (
     <div className='CharacterDetail'>
       <div className='content'>
         <img src={image} />
 
         <div className='name'>{name}</div>
+
+        <ul className='details'>
+          <li>
+            <label>Status</label>
+
+            <div>{status}</div>
+          </li>
+
+          <li>
+            <label>Species</label>
+
+            <div>{species}</div>
+          </li>
+
+          <li>
+            <label>Location</label>
+
+            <div>{locationName}</div>
+          </li>
+        </ul>
       </div>
     </div>
   )
